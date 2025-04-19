cc218460fe8a921badfa24ea6ccd5dae764faa89
stefansto <stefannuestojic@gmail.com>
2025-04-19 13:44:30 +0200

    Updated Exchange component to use the API

--- src/components/Exchange/Exchange.jsx
+++ src/components/Exchange/Exchange.jsx
@@ -3,24 +3,20 @@ import React from "react";
 const Exchange = (props) => {
 
     let newTransaction = {
-        id: 1,
         type: "Exchange",
-        currIn: null,
-        ammountIn: 0,
-        currOut: null,
-        ammountOut: 0,
-        rate: 1,
         date: null,
-        cashier: props.user
+        cashier: props.user,
+        currencyIn: null,
+        currencyInAmmount: null,
+        currencyOut: null,
+        currencyOutAmmount: null,
+        rate: null
     }
-
-    let optionA = 0;
-    let optionB = 0;
 
     let ammountText = 0;
     let rate = 0;
-    let elA = null;
-    let elB = null;
+    let selectedOptionFrom = null;
+    let selectedOptionTo = null;
     return(
         <>
             <div className="">
@@ -29,12 +25,13 @@ const Exchange = (props) => {
                     <select 
                         onChange={(e)=>{
                             const index = e.target.selectedIndex;
-                            elA = e.target.childNodes[index];
-                            optionA = elA.getAttribute('id');
+                            selectedOptionFrom = e.target.childNodes[index];
                             ammountText = document.getElementById('ammountText').value = 0;
                         }} 
-                        className='w-40 m-5 p-1 border rounded-md' defaultValue={'selectedA'}>
-                            <option className="block px-4 py-2 text-sm text-white bg-gray-900" id="0" hidden disabled value='selectedA'>Choose:</option>
+                        className='w-40 m-5 p-1 border rounded-md'
+                        defaultValue={'defaultSelectValueFrom'}
+                    >
+                        <option className="block px-4 py-2 text-sm text-white bg-gray-900" id="0" hidden disabled value='defaultSelectValueFrom'>Choose:</option>
                         {
                             props.curr.map(function(prop, index){
                                 return <option className="block px-4 py-2 text-sm text-white bg-gray-900" key={index} id={prop.id}>{prop.name} </option>
@@ -46,13 +43,12 @@ const Exchange = (props) => {
                     <select 
                         onChange={(e)=>{
                             const index = e.target.selectedIndex;
-                            elB = e.target.childNodes[index];
-                            optionB = elB.getAttribute('id');
+                            selectedOptionTo = e.target.childNodes[index];
                         }} 
                         className='w-40 m-5 p-1 border rounded-md'
-                        defaultValue={'selectedB'}
+                        defaultValue={'defaultSelectValueTo'}
                     >
-                        <option className="block px-4 py-2 text-sm text-white bg-gray-900" hidden disabled value='selectedB'>Choose:</option>
+                        <option className="block px-4 py-2 text-sm text-white bg-gray-900" hidden disabled value='defaultSelectValueTo'>Choose:</option>
                         {
                             props.curr.map(function(prop, index){
                                 return <option className="block px-4 py-2 text-sm text-white bg-gray-900" key={index} id={prop.id}>{prop.name}</option>
@@ -71,81 +67,73 @@ const Exchange = (props) => {
                     </div>
                 </div>
                     
-                    <button className="border rounded-md p-1 hover:bg-blue-900 cursor-pointer" 
-                            onClick={()=>{
-                                let newMoney = null;
-                                let exchangeError = false;
-                                let errorList = [];
+                <button
+                    className="border rounded-md p-1 hover:bg-blue-900 cursor-pointer" 
+                    onClick={()=>{
+                        let exchangeError = false;
+                        let errorList = [];
 
-                                if(/^\d*\.?\d*$/.exec(document.getElementById('rate').value) && document.getElementById('rate').value > 0){
-                                    rate = parseFloat(document.getElementById('rate').value);
-                                }
-                                else{
-                                    exchangeError = true; 
-                                    errorList.push('Invalid Rate!');
-                                }
+                        if(/^\d*\.?\d*$/.exec(document.getElementById('rate').value) && document.getElementById('rate').value > 0){
+                            rate = parseFloat(document.getElementById('rate').value);
+                        } else {
+                            exchangeError = true; 
+                            errorList.push('Invalid Rate!');
+                        }
 
-                                if(/^\d*\.?\d*$/.exec(document.getElementById('ammountText').value) && document.getElementById('ammountText').value > 0){
-                                    ammountText = parseFloat(document.getElementById('ammountText').value);
-                                }
-                                else{
-                                    exchangeError = true;
-                                    errorList.push('Invalid Ammount!')
-                                }
-                                
-                                if(elB===null || elA===null){
-                                    errorList.push('Please select your currencies!');
-                                    exchangeError = true;
-                                }
-                                else{
-                                    if(elA.value == elB.value){
-                                        errorList.push('Please select different currencies!');
-                                        exchangeError = true;
-                                    }
-                                    else{
-                                        newTransaction.currOut = elA.value;
-                                        newTransaction.currIn = elB.value;
-                                    }
-                                }
+                        if(/^\d*\.?\d*$/.exec(document.getElementById('ammountText').value) && document.getElementById('ammountText').value > 0){
+                            ammountText = parseFloat(document.getElementById('ammountText').value);
+                        } else {
+                            exchangeError = true;
+                            errorList.push('Invalid Ammount!')
+                        }
 
-                                if(exchangeError)console.log(errorList)
-                                else {
-                                    newMoney = props.curr.map(x => {
-                                        if(x.id == optionA){
-                                            if(x.ammount < ammountText){
-                                                errorList.push('Not enought funds in register');
-                                                exchangeError=true;
-                                            }
-                                            newTransaction.ammountOut = ammountText;
-                                            return {...x, ammount: x.ammount - ammountText }
-                                        }else if(x.id == optionB){
-                                            newTransaction.ammountIn = ammountText * rate;
-                                            return {...x, ammount: x.ammount + ammountText * rate}
-                                        }else{
-                                            return {...x};
-                                        }
-                                    })
-                                }
-                                
+                        if(selectedOptionTo===null || selectedOptionFrom===null){
+                            errorList.push('Please select your currencies!');
+                            exchangeError = true;
+                        } else {
+                            if(selectedOptionFrom.value == selectedOptionTo.value){
+                                errorList.push('Please select different currencies!');
+                                exchangeError = true;
+                            } else {
+                                newTransaction.currencyOut = selectedOptionFrom.value;
+                                newTransaction.currencyIn = selectedOptionTo.value;
+                            }
+                        }
+
                         if(!exchangeError){
-                            newTransaction.id = crypto.randomUUID();
                             newTransaction.rate = rate;
                             let dateAndTime = new Date();
                             newTransaction.date = dateAndTime.getDate() +'/'+ (dateAndTime.getMonth()+1) +'/'+ dateAndTime.getFullYear() + '  ' + dateAndTime.getHours() + ':' + dateAndTime.getMinutes() + ':' + dateAndTime.getSeconds();
-    
-                            props.setMoney(newMoney);
-                            props.setTransactions(newTransaction);
-                            props.onClose();
-                            alert('Successfully added: ' + newTransaction.ammountIn + ' ' + newTransaction.currIn);
-                        }
-                        else{
+                            newTransaction.currencyInAmmount = (ammountText * rate);
+                            newTransaction.currencyOutAmmount = ammountText;
+                            fetch(`${props.API_URL}/transaction`, {
+                                method: 'put',
+                                headers: {'Content-type': 'application/json'},
+                                body: JSON.stringify(newTransaction)
+                            })
+                            .then(response => response.json())
+                            .then(data => {
+                                if(data.message === 'Error'){
+                                alert('Database error');
+                                } else {
+                                    alert('Successfully added: ' + newTransaction.currencyInAmmount + ' ' + newTransaction.currencyIn);
+                                    props.refreshData();
+                                }
+                            })
+                            .catch((e)=>{
+                                alert('Server error, please try again later!');
+                            })
+                            .finally(()=>{
+                                props.onClose();
+                            })
+                        } else {
                             let txt = '';
                             errorList.forEach((x)=>{txt+=x});
                             alert(errorList);
                         }
-                        
-                    }}>Exchange</button>
-                
+                    }}>
+                    Exchange
+                </button>
             </div>
         </>
     );
